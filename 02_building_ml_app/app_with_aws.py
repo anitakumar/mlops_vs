cfcd22e39e8c4ff9c5283b0db112628663860a42
Anita Kumar <anitakumar>
2025-08-18 15:20:16 +0100

    Removed aws s3 download from the code and add it to jenkins jon insread 003 which will download from s3 and rebuild the docker image

--- 02_building_ml_app/app_with_aws.py
+++ 02_building_ml_app/app_with_aws.py
@@ -11,6 +11,21 @@ from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 
 
 local_model_path= 'model/house_price_model.pkl'
+bucket_name = 'visualmlops-ai-artifacts'  # Replace with your S3 bucket name
+s3_model_path = 'model/house_price_model.pkl'  # S3 path where the model will be stored
+
+
+def download_model_from_s3(bucket_name, s3_object_name, local_model_path):
+    """Download a file from S3 to a local path."""
+    s3 = boto3.client('s3') # Create an S3 client 
+    try:
+        s3.download_file(bucket_name, s3_object_name, local_model_path)
+        print(f"Model downloaded from S3 bucket '{bucket_name}' to '{local_model_path}'.")   
+    except FileNotFoundError:
+        print(f"The file {local_model_path} was not found.")
+    except NoCredentialsError:
+        print("Credentials not available.")      
+
 
 # Function to make predictions  
 def make_prediction(input_data):
@@ -29,6 +44,11 @@ def make_prediction(input_data):
 # Example input for prediction
 # print("Predicted House Price:", make_prediction(np.array([[2,1000,1,0,3]])))
 
+
+
+#download the model from S3
+print(f"Downloading the model from S3 bucket '{bucket_name}'...")
+download_model_from_s3(bucket_name, s3_model_path, local_model_path)
 # Create a Flask application
 app = Flask(__name__)
 
